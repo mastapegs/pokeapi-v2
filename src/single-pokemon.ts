c2b2bdef8c03b7ac6e4c46ec2b90ec5bd659f59e
Matthew Pagan <mastapegs01@gmail.com>
2021-01-03 00:29:01 -0500

    data!!!

--- src/single-pokemon.ts
+++ src/single-pokemon.ts
@@ -14,13 +14,13 @@ export default class SinglePokemon extends LitElement {
   @property({ attribute: false })
   singlePokeResponse = this.getSinglePokemonData();
 
-  private async getSinglePokemonData(): Promise<object> {
-    console.log(this.pokemon.url);
-    const response = await fetch(this.pokemon.url, {
-      headers: { 'Content-Type': 'application/json' },
-    });
+  private async getSinglePokemonData(): Promise<{
+    // eslint-disable-next-line camelcase
+    sprites: { front_default: string; back_default: string };
+  }> {
+    await new Promise(r => setTimeout(r, 0));
+    const response = await fetch(this.pokemon.url);
     const data = await response.json();
-    console.log(data);
     return data;
   }
 
@@ -31,7 +31,15 @@ export default class SinglePokemon extends LitElement {
         <p>Link: <a href=${this.pokemon.url}>${this.pokemon.url}</a></p>
         ${until(
           this.singlePokeResponse
-            .then(data => html`<pre>${data}</pre>`)
+            .then(
+              data =>
+                html`<details>
+                    <summary>Data</summary>
+                    <pre>${JSON.stringify(data, null, 2)}</pre>
+                  </details>
+                  <img src=${data.sprites.front_default} />
+                  <img src=${data.sprites.back_default} /> `
+            )
             .catch(error => html`<pre>${error}</pre>`),
           html`<p>Loading...</p>`
         )}
