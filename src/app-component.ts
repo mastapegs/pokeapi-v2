020a376c7936bf4fd59e7168ba92b8efdb192dcc
Matthew Pagan <mastapegs01@gmail.com>
2021-01-02 22:08:25 -0500

    initial commit - hello, world!

---
+++ src/app-component.ts
@@ -0,0 +1,10 @@
+import { LitElement, html, customElement } from "lit-element";
+
+@customElement('app-component')
+export default class AppComponent extends LitElement {
+  render() {
+    return html`
+      <h1>Hello, World!</h1>
+    `
+  }
+}
