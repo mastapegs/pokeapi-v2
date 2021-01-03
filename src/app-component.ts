a6883a52124c426fd184cfc179052e4ce473f32e
Matthew Pagan <mastapegs01@gmail.com>
2021-01-02 23:28:29 -0500

    correctly use until directive for async data

--- src/app-component.ts
+++ src/app-component.ts
@@ -1,8 +1,43 @@
-import { LitElement, html, customElement, TemplateResult } from 'lit-element';
+import {
+  LitElement,
+  html,
+  customElement,
+  TemplateResult,
+  property,
+} from 'lit-element';
+import { until } from 'lit-html/directives/until';
+
+interface PokemonResponse {
+  count: number | null;
+  next: string | null;
+  previous: string | null;
+  results: {
+    name: string;
+    url: string;
+  }[];
+}
 
 @customElement('app-component')
 export default class AppComponent extends LitElement {
+  @property() message = 'Hello, World!';
+
+  @property({ attribute: false })
+  pokemonResponse: Promise<PokemonResponse> = AppComponent.getData();
+
+  private static async getData(): Promise<PokemonResponse> {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+    const data = await response.json();
+    console.log(data);
+    return data;
+  }
+
   render(): TemplateResult {
-    return html` <h1>Hello, World!</h1> `;
+    return html` <h1>${this.message}</h1>
+      ${until(
+        this.pokemonResponse.then(
+          data => html`<pre>${JSON.stringify(data, null, 2)}</pre>`
+        ),
+        html`<p>Loading...</p>`
+      )}`;
   }
 }
